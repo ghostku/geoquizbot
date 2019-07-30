bb14662c2b8214261eb1f1ea8d7f6c64af907bea
GhostKU <dev@ghostku.com>
2019-07-30 22:50:16 +0300

    TRY

--- app/__init__.py
+++ app/__init__.py
@@ -2,31 +2,31 @@ import os
 
 from flask import Flask, request
 
-# import telebot
+import telebot
 
 TOKEN = '915055480:AAF8d8fTTeD6QaPUs3aVOTcsUtxbTVYwTYE'
-# bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
 
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-# @bot.message_handler(func=lambda message: True, content_types=['text'])
-# def echo_message(message):
-#     bot.reply_to(message, message.text)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-# @app.route('/' + TOKEN, methods=['POST'])
-# def getMessage():
-#     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-#     return "!", 200
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
 @app.route("/")
 def webhook():
-    # bot.remove_webhook()
-    # bot.set_webhook(url='https://geoquizbot.herokuapp.com/' + TOKEN)
+    bot.remove_webhook()
+    bot.set_webhook(url='https://geoquizbot.herokuapp.com/' + TOKEN)
     return "!", 200
