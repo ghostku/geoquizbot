2a3b7c89b01601d495ab81438efa78fe36b5b4d1
GhostKU <dev@ghostku.com>
2019-07-31 02:57:53 +0300

    DEv Add reset command

--- app/__init__.py
+++ app/__init__.py
@@ -78,6 +78,10 @@ class Questions(object):
 def start(message):
     bot.reply_to(message, "Hello, " + message.from_user.first_name)
 
+@bot.message_handler(commands=["reset"])
+def reset(message):
+    storage.delete(message.chat.id)
+    bot.reply_to(message, "Состояние сброшено")
 
 @bot.message_handler(func=lambda message: True, content_types=["text"])
 def echo_message(message):
